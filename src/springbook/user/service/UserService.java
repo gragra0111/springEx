203c5136fbf9aa77d7fd161b0ca5b448672ee312
gragra0111 <gragra0111@gmail.com>
2016-01-21 13:27:31 +0900

    [2016.01.21]p.350~p.370

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -1,6 +1,12 @@
 package springbook.user.service;
 
+import java.sql.Connection;
 import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
@@ -9,37 +15,37 @@ import springbook.user.domain.User;
 public class UserService {
 	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
 	public static final int MIN_RECOMMEND_FOR_GOLD = 30;
-	
+	private DataSource dataSource;
 	UserDao userDao;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 	
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
 	
-	public void upgradeLevels() {
-		List<User> users = userDao.getAll();
-		for(User user : users) {
-			/*
-			 * Boolean changed = null;
-			if(user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-				user.setLevel(Level.SILVER);
-				changed = true;
-			} else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-				user.setLevel(Level.GOLD);
-				changed = true;
-			} else if(user.getLevel() == Level.GOLD) {
-				changed = false;
-			} else {
-				changed = false;
+	public void upgradeLevels() throws Exception {
+		TransactionSynchronizationManager.initSynchronization();	//트랜잭션 동기화 관리자를 이용해 동기화 작업을 초기화한다.
+		Connection c = DataSourceUtils.getConnection(dataSource);	//DB커넥션을 생성하고 (DB 커넥션 생성과 동기화를 함께 해주는 유틸리티 메소드)
+		c.setAutoCommit(false);										//트랜잭션을 시작한다. 이후의 DAO 작업은 모두 여기서 시작한 트랜잭션 안에서 진행된다.
+		
+		try {
+			List<User> users = userDao.getAll();
+			for(User user : users) {
+				if(canUpgradeLevel(user)) {
+					upgradeLevel(user);
+				}
 			}
-			if(changed) {
-				userDao.update(user);
-			}
-			 * 
-			 * */
-			if(canUpgradeLevel(user)) {
-				upgradeLevel(user);
-			}
+			c.commit();	//정상적으로 작업을 마치면 트랜잭션 커밋
+		} catch(Exception e) {
+			c.rollback();	//예외가 발생하면 롤백한다.
+			throw e;
+		} finally {
+			DataSourceUtils.releaseConnection(c, dataSource);	//스프링 유틸리티 메소드를 이용해 DB 커넥션을 안전하게 닫는다.
+			TransactionSynchronizationManager.unbindResource(this.dataSource);	//동기화 작업 종료
+			TransactionSynchronizationManager.clearSynchronization();			//동기화 작업 정리
 		}
 	}
 
@@ -53,7 +59,7 @@ public class UserService {
 		}
 	}
 
-	private void upgradeLevel(User user) {
+	protected void upgradeLevel(User user) {
 		user.upgradeLevel();
 		userDao.update(user);
 	}
--- src/springbook/user/service/UserServiceTest.java
+++ src/springbook/user/service/UserServiceTest.java
@@ -3,13 +3,16 @@ package springbook.user.service;
 import static springbook.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserService.MIN_RECOMMEND_FOR_GOLD;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -26,19 +29,22 @@ public class UserServiceTest {
 	UserService userService;
 	@Autowired
 	UserDao userDao;
+	@Autowired
+	DataSource dataSource;
 	List<User> users;
 	
 	@Before
 	public void setUp() {
 		users = Arrays.asList(
-				new User("PARK","박영진","1",Level.BASIC,MIN_LOGCOUNT_FOR_SILVER-1,0),
-				new User("GOO","구본식","2",Level.BASIC,MIN_LOGCOUNT_FOR_SILVER,0),
-				new User("JUNG","정광운","3",Level.SILVER,60,MIN_RECOMMEND_FOR_GOLD-1),
-				new User("GANG","강유성","4",Level.SILVER,60,MIN_RECOMMEND_FOR_GOLD),
-				new User("YOON","윤성진","5",Level.GOLD,100,Integer.MAX_VALUE)
+				new User("1PARK","박영진","1",Level.BASIC,MIN_LOGCOUNT_FOR_SILVER-1,0),
+				new User("2GOO","구본식","2",Level.BASIC,MIN_LOGCOUNT_FOR_SILVER,0),
+				new User("3JUNG","정광운","3",Level.SILVER,60,MIN_RECOMMEND_FOR_GOLD-1),
+				new User("4GANG","강유성","4",Level.SILVER,60,MIN_RECOMMEND_FOR_GOLD),
+				new User("5YOON","윤성진","5",Level.GOLD,100,Integer.MAX_VALUE)
 		);
 	}
 	
+/*
 	@Test
 	public void upgradeLevels() {
 		userDao.deleteAll();
@@ -54,7 +60,8 @@ public class UserServiceTest {
 		checkLevelUpgraded(users.get(3), true);
 		checkLevelUpgraded(users.get(4), false);
 	}
-
+*/
+/*
 	@Test
 	public void add() {
 		userDao.deleteAll();
@@ -72,7 +79,7 @@ public class UserServiceTest {
 		assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
 		assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
 	}
-	
+*/	
 	private void checkLevelUpgraded(User user, Boolean upgraded) {
 		User userUpdate = userDao.get(user.getId());
 		if(upgraded) {
@@ -80,7 +87,43 @@ public class UserServiceTest {
 		} else {
 			assertThat(userUpdate.getLevel(), is(user.getLevel()));
 		}
-		
 	}
 	
+	//UserService의 테스트용 대역 클래스
+	static class TestUserService extends UserService {
+		private String id;
+		
+		private TestUserService(String id) {
+			this.id = id;
+		}
+		
+		protected void upgradeLevel(User user) {
+			System.out.println(user.getId() + " : " + id);
+			if(user.getId().equals(this.id)) throw new TestUserServiceException();
+			super.upgradeLevel(user);
+		}
+	}
+	
+	static class TestUserServiceException extends RuntimeException {	
+	}
+	
+	@Test
+	public void upgradeAllOrNothing() throws Exception {
+		UserService testUserService = new TestUserService(users.get(3).getId());	//예외를 발생시킬 네 번째 사용자의 id를 넣어서 테스트용 UserService 대역 오브젝트를 생성한다.
+		testUserService.setUserDao(this.userDao);	//userDao를 수동 DI 해준다.
+		testUserService.setDataSource(this.dataSource);
+		userDao.deleteAll();
+		for(User user : users) {
+			userDao.add(user);
+		}
+		
+		try {
+			testUserService.upgradeLevels();
+			fail("TestUserServiceException expected");	//TestUserService는 업그레이드 작업 중에 예외가 발생해야 한다. 정상 종료라면 문제가 있으니 실패
+		} catch(TestUserServiceException e) {	//TestUserService가 던져주는 예외를 잡아서 계속 진행되도록 한다. 그 외의 예외라면 테스트 실패
+			
+		}
+		checkLevelUpgraded(users.get(1), false);	//예외가 발생하기 전에 레벨 변경이 있었던 사용자의 레벨이 처음 상태로 바뀌었나 확인
+	}
+
 }
